1aaa3e6b38442a654d60737da08bb68e6d80bde5
weiwenwei <15027128561@163.com>
2021-11-23 14:11:27 +0800

    first commit

---
+++ src/main/java/com/wei/emos/wx/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.wei.emos.wx.controller;
+
+import com.wei.emos.wx.common.util.R;
+import com.wei.emos.wx.controller.form.TestSayHelloForm;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * @author www
+ * @date 2021/11/22 19:23
+ * @description: 测试方法，测试swagger2的实现
+ */
+
+@RestController
+@RequestMapping("/test")
+@Api("测试web接口")
+public class TestController {
+
+    @PostMapping("/sayHello")
+    @ApiOperation("测试方法内部")
+    public R test(@Valid @RequestBody TestSayHelloForm form) {
+        return R.ok().put("message", "数据，你好swagger " + form.getName());
+    }
+}
